c0402d5b15d3cfa00756f36510d0da9cabbbc88f
cetmoe <cetmoe@gmail.com>
2021-03-30 04:02:49 +0200

    last commit before travel

--- website/app.py
+++ website/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, abort, jsonify, url_for, redirect
+from flask import Flask, render_template, abort, url_for, redirect, session
 from authlib.integrations.flask_client import OAuth
 from .config import BLIZZ_CLIENT_ID, BLIZZ_CLIENT_SECRET
 from jinja2 import TemplateNotFound
@@ -14,7 +14,7 @@ oauth.register(
     client_secret=BLIZZ_CLIENT_SECRET,
     authorize_url='https://eu.battle.net/oauth/authorize',
     access_token_url='https://eu.battle.net/oauth/token',
-    api_base_url=''
+    api_base_url='eu.battle.net/'
 )
 
 blizzard = oauth.blizzard
@@ -22,6 +22,8 @@ blizzard = oauth.blizzard
 
 @app.route('/')
 def home():
+    if 'token' in session:
+        print(session['token'])
     try:
         return render_template('index.html')
     except TemplateNotFound:
@@ -30,15 +32,21 @@ def home():
 
 @app.route('/login')
 def login():
-    print(True)
     redirect_uri = url_for('authorize', _external=True)
-    print(redirect_uri)
     return blizzard.authorize_redirect(redirect_uri)
 
 
 @app.route('/authorize')
 def authorize():
     token = blizzard.authorize_access_token()
+
+    session['token'] = token
+
     # you can save the token into database
-    profile = blizzard.get('/oauth/userinfo', token=token)
+    profile = blizzard.get('https://eu.api.blizzard.com/profile/user/wow', token=token)
     return redirect('/')
+
+
+@app.route('/profile')
+def profile():
+    return 'profile'
